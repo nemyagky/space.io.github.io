3a94251f5b26c97b04bfaf71ba9d636f8d7a7324
magmiv <48682276+magmiv@users.noreply.github.com>
2019-04-22 16:24:26 +0300

    Update images.js

--- js/images.js
+++ js/images.js
@@ -3,7 +3,7 @@ var images = {};
 (function createImages() {
 alert(123)
 	function createImage(src) {
-		var diredtory = "/imgs/";
+		var diredtory = "../imgs/";
 
 		newImage = new Image();
 	  newImage.src = diredtory + src;
